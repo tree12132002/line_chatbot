b7daebe1394b3f9431ae867ce57cbdccba39d09a
tree12132002 <tree12132002@hotmail.com>
2022-06-17 19:30:27 +0800

    feat: add home page

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const routes = require('./routes')
 const webhookRouter = require('./routes/webhook')
+const exphbs = require('express-handlebars')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -8,7 +10,12 @@ if (process.env.NODE_ENV !== 'production') {
 const app = express()
 const PORT = process.env.PORT || 3000
 
+// handlebars
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.use('/webhook', webhookRouter)
+app.use(routes)
 app.listen(PORT, () => {
   console.log(`Express is listening on localhost:${PORT}`)
 })
